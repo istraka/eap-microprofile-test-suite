a677c879638b4b1947a414a079131a6147f16b90
Ivan Straka <istraka@redhat.com>
2020-01-04 20:39:03 +0100

    reproducers

---
+++ microprofile-health/src/test/java/org/jboss/eap/qe/microprofile/health/integration/Reproducers.java
@@ -0,0 +1,141 @@
+package org.jboss.eap.qe.microprofile.health.integration;
+
+import io.restassured.http.ContentType;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.as.arquillian.api.ServerSetup;
+import org.jboss.as.arquillian.api.ServerSetupTask;
+import org.jboss.as.arquillian.container.ManagementClient;
+import org.jboss.eap.qe.microprofile.health.tools.HealthUrlProvider;
+import org.jboss.eap.qe.microprofile.tooling.server.configuration.creaper.ManagementClientProvider;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.wildfly.extras.creaper.core.online.OnlineManagementClient;
+import org.wildfly.extras.creaper.core.online.operations.admin.Administration;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.get;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+
+@RunWith(Arquillian.class)
+@RunAsClient
+@ServerSetup({ MicroProfileFTSetupTask.class, Reproducers.SetupTask.class })
+public class Reproducers extends FailSafeCDIHealthBaseTest {
+
+    @Deployment(testable = false)
+    public static Archive<?> deployment() {
+        WebArchive war = ShrinkWrap.create(WebArchive.class, Reproducers.class + ".war")
+                .addClasses(FailSafeDummyService.class, CDIBasedLivenessHealthCheck.class, CDIBasedReadinessHealthCheck.class)
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+        return war;
+    }
+
+    @Override
+    void setConfigProperties(boolean live, boolean ready, boolean inMaintanance, boolean readyInMainenance) throws Exception {
+        OnlineManagementClient client = ManagementClientProvider.onlineStandalone();
+        client.execute(String.format(
+                "/subsystem=microprofile-config-smallrye/config-source=props:write-attribute(name=properties, value={%s=%s,%s=%s,%s=%s,%s=%s}",
+                FailSafeDummyService.LIVE_CONFIG_PROPERTY, live,
+                FailSafeDummyService.READY_CONFIG_PROPERTY, ready,
+                FailSafeDummyService.IN_MAINTENANCE_CONFIG_PROPERTY, inMaintanance,
+                FailSafeDummyService.READY_IN_MAINTENANCE_CONFIG_PROPERTY, readyInMainenance));
+        new Administration(client).reload();
+    }
+
+    /**
+     * Setup a microprofile-config-smallrye subsystem to obtain values from properties defined in the subsystem
+     */
+    public static class SetupTask implements ServerSetupTask {
+        @Override
+        public void setup(ManagementClient managementClient, String s) throws Exception {
+            ManagementClientProvider.onlineStandalone()
+                    .execute("/subsystem=microprofile-config-smallrye/config-source=props:add");
+        }
+
+        @Override
+        public void tearDown(ManagementClient managementClient, String s) throws Exception {
+            ManagementClientProvider.onlineStandalone()
+                    .execute("/subsystem=microprofile-config-smallrye/config-source=props:remove");
+        }
+    }
+
+
+    @Test
+    @RunAsClient
+    public void retryCallsSucceededCounter() throws Exception {
+        setConfigProperties(false, true, true, false);
+        // TODO Java 11 Map<String, String> liveCheck = Map.of( "name", "dummyLiveness", "status", "DOWN");
+        Map<String, String> liveCheck = Collections.unmodifiableMap(new HashMap<String, String>() {
+            {
+                put("name", "dummyLiveness");
+                put("status", "DOWN");
+            }
+        });
+        Map<String, String> readyCheck = Collections.unmodifiableMap(new HashMap<String, String>() {
+            {
+                put("name", "dummyReadiness");
+                put("status", "DOWN");
+            }
+        });
+        get(HealthUrlProvider.healthEndpoint()).then()
+                .statusCode(503)
+                .contentType(ContentType.JSON)
+                .header("Content-Type", containsString("application/json"))
+                .body("status", is("DOWN"),
+                        "checks", hasSize(2),
+                        "checks", containsInAnyOrder(liveCheck, readyCheck));
+
+        MetricsChecker.get()
+                .validateSimulationCounter(FailSafeDummyService.MAX_RETRIES + 1) // 1 call + N retries
+                .validateInvocationsTotal(1)
+                .validateInvocationsFailedTotal(1)
+                .validateRetryRetriesTotal(FailSafeDummyService.MAX_RETRIES) // N retries
+                .validateRetryCallsFailedTotal(1)
+                .validateRetryCallsSucceededTotal(0);
+    }
+
+
+    @Test
+    @RunAsClient
+    public void fallbackCounter() throws Exception {
+        setConfigProperties(false, true, true, false);
+        // TODO Java 11 Map<String, String> liveCheck = Map.of( "name", "dummyLiveness", "status", "DOWN");
+        Map<String, String> liveCheck = Collections.unmodifiableMap(new HashMap<String, String>() {
+            {
+                put("name", "dummyLiveness");
+                put("status", "DOWN");
+            }
+        });
+        Map<String, String> readyCheck = Collections.unmodifiableMap(new HashMap<String, String>() {
+            {
+                put("name", "dummyReadiness");
+                put("status", "DOWN");
+            }
+        });
+        get(HealthUrlProvider.healthEndpoint()).then()
+                .statusCode(503)
+                .contentType(ContentType.JSON)
+                .header("Content-Type", containsString("application/json"))
+                .body("status", is("DOWN"),
+                        "checks", hasSize(2),
+                        "checks", containsInAnyOrder(liveCheck, readyCheck));
+
+        MetricsChecker.get()
+                .validateSimulationCounter(FailSafeDummyService.MAX_RETRIES + 1) // 1 call + N retries
+                .validateInvocationsTotal(1)
+                .validateInvocationsFailedTotal(1)
+                .validateRetryRetriesTotal(FailSafeDummyService.MAX_RETRIES) // N retries
+                .validateFallbackCallsTotal(1);
+    }
+}
